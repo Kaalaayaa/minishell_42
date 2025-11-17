c5b90caf71a5fb0da6b11cbf61b99da13425fd97
Pradyun <pradyundangwal@gmail.com>
2025-11-17 11:52:25 +0100

    144/146

--- src/utils/shell_init.c
+++ src/utils/shell_init.c
@@ -5,6 +5,7 @@ void	shell_init(t_shell *shell, char **envp)
     if (!shell)
         return ;
     shell->env_list = NULL;
+    shell->in_pipe = false;
     if (envp)
         shell->env_list = env_list_init(envp);
     shell->exit_status = 0;
